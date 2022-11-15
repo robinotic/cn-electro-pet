f1af465090aafdb7ebbe6b3bb041cfcede7bd371
robinotic <robin.hemuss@gmx.com>
2022-11-15 12:17:59 +0000

    Basic layout of the Pet class, deleted some comments

--- electropet-class-v1.js
+++ electropet-class-v1.js
@@ -13,14 +13,6 @@ class Pet {
         this.name = name;
         return `Your pet's name is ${this.name}. It is a ${this.colour} ${this.type}. It is ${this.age} old and has a ${this.temperament}. It needs to be fed ${this.food} within ${this.foodLength}.`;
     }
-    // feedPet () {
-    //     lastfed = lastFed;
-    //     if {lastFed < foodLength }
-    // }
-    // const feedPet = (this.name, this.preffood, this.foodLength) => {
-    //     let lastFed = lastFed
-    //     if (lastFed < foodLength)
-    // }
 }
 const pet1 = new Pet("pet 1","cat","yellow","angry", 2, "biscuits", 2, "cuddling")
 
